7c2e4d8a0f9ae1ec39033cfd7e3b55321e898a14
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2024-10-10 10:23:38 +0200

    [ADD] a new test

--- src/test1.js
+++ src/test1.js
@@ -1,18 +1,53 @@
-const crypto = require('crypto');
+import express from 'express';
+import fs from 'fs';
 
-const keyPart1 = "akdsjfh";
-const keyPart2 = "9fas8df";
-const keyPart3 = "asl89asj";
-const hiddenKey = keyPart1 + keyPart2 + keyPart3;
+const app = express();
 
-function getSecretKey() {
-    const key = hiddenKey.split('').reverse().join('');
-    return crypto.createHmac('sha256', key).digest('hex');
-}
+app.get('/secret', (req, res) => {
+    const password = req.query.password;
 
-function makeRequest() {
-    const apiKey = getSecretKey();
-    console.log("Utilisation de la clé API masquée : ", apiKey);
-}
+    if (password === '7WK5T79u5mIzjIXXi2oI9Fglmgivv7RAJ7izyj9tUyQ') {
+        res.send('Accès autorisé');
+    } else {
+        res.send('Accès refusé');
+    }
+});
 
-makeRequest();
+app.get('/readfile', (req, res) => {
+    const data = fs.readFileSync('/path/to/file.txt', 'utf8');
+    res.send(data);
+});
+
+import mysql from 'mysql2';
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootpassword',
+    database: 'canditrack'
+});
+
+app.get('/user', (req, res) => {
+    const userId = req.query.id;
+    connection.query(`SELECT * FROM users WHERE id = ${userId}`, (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
+});
+
+app.get('/error', (req, res) => {
+    const someOperation = () => {
+        throw new Error('Quelque chose a mal tourné');
+    };
+
+    someOperation();
+    res.send('Tout va bien');
+});
+
+app.get('/async-error', async (req, res) => {
+    const result = await fetch('https://api.example.com/data');
+    res.send(await result.json());
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
